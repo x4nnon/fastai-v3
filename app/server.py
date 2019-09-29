667c5982c06c631718b2f24857cfe509dd9ae4ce
x4nnon <55958043+x4nnon@users.noreply.github.com>
2019-09-29 19:51:10 +0100

    Update server.py
    change URL to export and change of class names

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1frsi7XjfNZdS-hxA1pJniFwkca9_Eqvz?alt=media&key=AIzaSyBaxwCP_niL-RzmamiVTltqdTk-HHPoUEQ'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Up', 'Down', 'No']
 path = Path(__file__).parent
 
 app = Starlette()
